73ddf222fc0af3e81a4cc48ce2f113ab2a39229a
Felix-Cadiz <88756231+Felix-Cadiz@users.noreply.github.com>
2023-02-09 13:01:18 -0800

    block 13 solutions added

---
+++ Unit-2/block13_conditionals_and_comparisons/guided_practice/block13_guided_practice_solutions.js
@@ -0,0 +1,12 @@
+// Solution for Block 13 Guided Practice
+
+// Inputs will vary between the following:
+// [0, 1, 5, 16, 256, -5, -5555555555555]
+
+let input = 0;
+
+if (input % 2 === 0) {
+    console.log("even")
+} else {
+    console.log("odd")
+};
