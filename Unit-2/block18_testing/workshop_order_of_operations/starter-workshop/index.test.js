960a9d7470b1891c8a24aa1773ec6b347a5bc49d
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-02-24 12:46:30 -0600

    removed starter code

--- Unit-2/block18_testing/workshop_order_of_operations/starter-workshop/index.test.js
+++
@@ -1,8 +0,0 @@
-const { orderOfOperations } = require('./index');
-
-//test the function orderOfOperations which takes three numbers as arguments and returns the result of the operation
-test('orderOfOperations function exists and gives the correct result', () => {
-    expect(orderOfOperations).toBeDefined();
-    expect(orderOfOperations(1, 2, 3)).toEqual(7);
-});
-
--- Unit-2/block20_DOM/workshop_tdd_webpage/starter-workshop/index.js
+++ Unit-2/block20_DOM/workshop_tdd_webpage/starter-workshop/index.js
@@ -10,33 +10,7 @@ const users = [
 ];
 
 function main() {
-    //grab the div with the id of "root"
-    const root = document.getElementById("root");
 
-    //create a new h1 element that says "USERS"
-    const h1 = document.createElement("h1");
-    h1.innerHTML = "USERS";
-
-    //append the h1 element to the root div
-    root.appendChild(h1);
-
-    //create a new ul element
-    const ul = document.createElement("ul");
-
-    //loop through the users array
-    for (let i = 0; i < users.length; i++) {
-        //create a new li element
-        const li = document.createElement("li");
-
-        //set the innerHTML of the li element to the name of the user
-        li.innerHTML = users[i].name;
-
-        //append the li element to the ul element
-        ul.appendChild(li);
-    }
-
-    //append the ul element to the root div
-    root.appendChild(ul);
 }
 
 //call the main function
