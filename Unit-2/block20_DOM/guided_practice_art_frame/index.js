b06e75e84c73406e19958fc48912a64059965d21
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-02-23 20:51:46 -0600

    added main() function

--- Unit-2/block20_DOM/guided_practice_art_frame/index.js
+++ Unit-2/block20_DOM/guided_practice_art_frame/index.js
@@ -1,27 +1,33 @@
-// grab the root div
-const root = document.getElementById("root");
+const IMG_FILE = "./assets/mona_lisa.jpg";
 
-// create a new h1 element that says "MONA LISA"
-const h1 = document.createElement("h1");
-h1.innerHTML = "MONA LISA";
+function main() {
+    // grab the root div
+    const root = document.getElementById("root");
 
-// append the h1 element to the root div
-root.appendChild(h1);
+    // create a new h1 element that says "MONA LISA"
+    const h1 = document.createElement("h1");
+    h1.innerHTML = "MONA LISA";
 
-// create a new img element
-const img = document.createElement("img");
+    // append the h1 element to the root div
+    root.appendChild(h1);
 
-// set the src attribute of the img element to the mona lisa image
-img.src = './assets/mona_lisa.jpg';
+    // create a new img element
+    const img = document.createElement("img");
 
-// append the img element to the root div
-root.appendChild(img);
+    // set the src attribute of the img element to the mona lisa image
+    img.src = IMG_FILE;
 
-// create a new p element
-const p = document.createElement("p");
+    // append the img element to the root div
+    root.appendChild(img);
 
-// set the innerHTML of the p element to the description of the mona lisa
-p.innerHTML = "The Mona Lisa is a half-length portrait painting by the Italian Renaissance artist Leonardo da Vinci. It is considered an archetypal masterpiece of the Italian Renaissance, and has been described as the best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world. The painting's novel qualities include the subject's enigmatic expression, the monumentality of the composition, the subtle modelling of forms, and the atmospheric illusionism. It is regarded as Leonardo's masterpiece and the best known work of Renaissance painting. It has been in the Louvre in Paris since 1797, acquired by King Louis XVI of France and early in his reign, and is the world's most visited art object."
+    // create a new p element
+    const p = document.createElement("p");
 
-// append the p element to the root div
-root.appendChild(p);
+    // set the innerHTML of the p element to the description of the mona lisa
+    p.innerHTML = "The Mona Lisa is a half-length portrait painting by the Italian Renaissance artist Leonardo da Vinci. It is considered an archetypal masterpiece of the Italian Renaissance, and has been described as the best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world. The painting's novel qualities include the subject's enigmatic expression, the monumentality of the composition, the subtle modelling of forms, and the atmospheric illusionism. It is regarded as Leonardo's masterpiece and the best known work of Renaissance painting. It has been in the Louvre in Paris since 1797, acquired by King Louis XVI of France and early in his reign, and is the world's most visited art object."
+
+    // append the p element to the root div
+    root.appendChild(p);
+}
+
+main();
