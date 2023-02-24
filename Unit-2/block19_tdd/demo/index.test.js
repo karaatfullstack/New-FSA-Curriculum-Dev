1f4c58a97c1acbc1515b12e1cd7b237ea968eb18
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-02-23 23:29:08 -0600

    works except updating checkbox

--- Unit-2/block19_tdd/demo/index.test.js
+++ Unit-2/block19_tdd/demo/index.test.js
@@ -1,9 +1,13 @@
 const {calculateAge, canDrive, calculateCanDrive} = require('./index');
 
+/* Testing the function calculateAge. It is testing that if the person is born in 1990, it should
+return 32. */
 test('calculateAge should return the age of a person', () => {
     expect(calculateAge(1990)).toBe(32);
 });
 
+/* This is a test case. It is testing the function canDrive. It is testing that if the person is 18 or
+older, it should return true. */
 test('canDrive should return true if the person is 18 or older', () => {
     expect(canDrive(18)).toBe(true);
 });
--- Unit-2/block21_classes_and_prototypes/demo/index.js
+++ Unit-2/block21_classes_and_prototypes/demo/index.js
@@ -7,7 +7,7 @@ const animal = {
 
 /* Creating an object called dog with a method called bark. */
 const dog = {
-    bark: function() {
+    woof: function() {
         console.log("Woof!");
     }
 };
@@ -17,3 +17,56 @@ dog.__proto__ = animal;
 
 /* Calling the eat method from the animal object. */
 dog.eat(); // Output: "nom nom nom"
+
+// Define a base class for pets
+class Pet {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    eat() {
+        return `${this.name} is ${this.age} years old.`;
+    }
+}
+
+// Define a subclass for dogs
+class Dog extends Pet {
+    constructor(name, age, breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    bark() {
+        return `${this.name} is barking!`;
+    }
+}
+
+// Define a subclass for cats
+class Cat extends Pet {
+    constructor(name, age, furColor) {
+        super(name, age);
+        this.furColor = furColor;
+    }
+
+    meow() {
+        return `${this.name} is meowing!`;
+    }
+}
+
+// Create some instances of the classes
+const pet1 = new Pet("Spot", 3);
+console.log(pet1.eat()); // Output: "Spot is 3 years old."
+
+const pet2 = new Dog("Rover", 2, "Labrador");
+console.log(pet2.eat()); // Output: "Rover is 2 years old."
+console.log(pet2.bark()); // Output: "Rover is barking!"
+
+const pet3 = new Cat("Fluffy", 1, "white");
+console.log(pet3.eat()); // Output: "Fluffy is 1 year old."
+console.log(pet3.meow()); // Output: "Fluffy is meowing!"
+
+// Check if the instances are instances of their respective classes
+console.log(pet1 instanceof Pet); // Output: true
+console.log(pet2 instanceof Dog); // Output: true
+console.log(pet3 instanceof Cat); // Output: true
