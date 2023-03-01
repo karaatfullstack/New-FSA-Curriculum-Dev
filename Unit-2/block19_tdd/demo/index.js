c3fe353414fb3ed82cee90144ec26eae47bd937a
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-02-28 18:24:20 -0600

    block 19 tdd guided practice update

--- Unit-2/block19_tdd/demo/index.js
+++ Unit-2/block19_tdd/demo/index.js
@@ -1,9 +1,13 @@
 function generateRandomPhoneNumber(a, b, c) {
-    const areaCode = Math.floor(Math.random() * (a * 100)) + 100;
-    const prefix = Math.floor(Math.random() * (b * 100)) + 100;
-    const lineNumber = Math.floor(Math.random() * (c * 1000)) + 1000;
-    return `(${areaCode}) ${prefix}-${lineNumber}`;
+  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+    throw new Error('All parameters must be numbers');
   }
 
+  const areaCode = Math.floor(Math.random() * (a * 100)) + 100;
+  const prefix = Math.floor(Math.random() * (b * 100)) + 100;
+  const lineNumber = Math.floor(Math.random() * (c * 1000)) + 1000;
+  return `(${areaCode}) ${prefix}-${lineNumber}`;
+}
 
-module.exports = {generateRandomPhoneNumber};
+
+module.exports = { generateRandomPhoneNumber };
