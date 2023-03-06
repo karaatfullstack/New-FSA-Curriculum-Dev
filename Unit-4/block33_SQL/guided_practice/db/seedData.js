ee990065d2ca862fd73d8147257dfc7612bdc31f
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-03-06 16:34:22 -0600

    guided practice bug

--- Unit-4/block33_SQL/guided_practice/db/seedData.js
+++ Unit-4/block33_SQL/guided_practice/db/seedData.js
@@ -95,19 +95,22 @@ async function createInitialArticles() {
         title: 'How to make a great first impression',
         description: 'You only get one chance to make a first impression. Here are some tips to make sure you make the best first impression possible.',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        author_id: 1
+        author_id: 1,
+        keywords: ['first impression', 'impressing people', 'making a good first impression']
       },
       {
         title: 'Saving Money on Groceries',
         description: 'Groceries can be expensive. Here are some tips to help you save money on groceries.',
         content: 'At imperdiet dui accumsan sit amet nulla facilisi. Dignissim suspendisse in est ante in nibh mauris cursus. In iaculis nunc sed augue lacus viverra vitae congue eu. Netus et malesuada fames ac. Enim nulla aliquet porttitor lacus. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Nibh praesent tristique magna sit amet purus. Rhoncus aenean vel elit scelerisque. Id volutpat lacus laoreet non curabitur gravida arcu. Convallis aenean et tortor at risus viverra adipiscing at in. Bibendum arcu vitae elementum curabitur vitae nunc sed. Id consectetur purus ut faucibus pulvinar elementum integer enim neque. At risus viverra adipiscing at in tellus integer feugiat. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Proin sed libero enim sed faucibus turpis in. In pellentesque massa placerat duis ultricies lacus sed turpis. Dolor purus non enim praesent elementum. Eu facilisis sed odio morbi quis commodo odio aenean.',
-        author_id: 1
+        author_id: 1,
+        keywords: ['saving money', 'groceries', 'saving money on groceries']
       },
       {
         title: 'Making your Home More Eco-Friendly',
         description: 'There are many ways to make your home more eco-friendly. Here are some tips to help you get started.',
         content: 'Sit amet facilisis magna etiam tempor orci eu lobortis elementum. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Dolor purus non enim praesent elementum. Arcu risus quis varius quam quisque id diam. Id eu nisl nunc mi ipsum faucibus vitae aliquet. Vitae congue eu consequat ac felis donec. Ullamcorper malesuada proin libero nunc. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue. Et malesuada fames ac turpis egestas sed tempus. Duis tristique sollicitudin nibh sit amet commodo nulla facilisi. Viverra vitae congue eu consequat. In massa tempor nec feugiat nisl pretium fusce id velit.',
-        author_id: 1
+        author_id: 1,
+        keywords: ['eco-friendly', 'green living', 'making your home more eco-friendly']
       },
     ];
     const articles = await Promise.all(articlesToCreate.map(createSeedArticle));
