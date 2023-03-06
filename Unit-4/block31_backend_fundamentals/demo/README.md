4b146746a13e3a5aa19cf8fb0541049a33e98017
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-03-06 10:11:15 -0600

    block 31 cleanup

--- Unit-4/block31_backend_fundamentals/demo/README.md
+++ Unit-4/block31_backend_fundamentals/demo/README.md
@@ -2,10 +2,14 @@
 
 ## Overview
 
-In this demo, we will be building a RESTful API with Express. We will be using the [Express](https://expressjs.com/) framework to build our API. We will also be using [Postman](https://www.getpostman.com/) to test our API.
+In this demo, we will be building a RESTful API with Express. 
+
+First, demonstrate a GET request without Express.js. Coding sample found in the `index.js` file.
+
+Then, demonstrate a GET request with Express.js. Coding sample found in the `server.js` file.
+
+We will be using the [Express](https://expressjs.com/) framework to build our API. 
+
+In the future, we will be using [Postman](https://www.getpostman.com/) to test our API, but all of today's examples can be done in the browser. 
 
 Begin by cloning this repo, then run `npm install` to install the dependencies, and then `npm start` to start the server. Your server should be running at `http://localhost:8080`.
-
-## GET with Express
-
-In this section, we will be building a GET route with Express. We will be using the `GET` method to retrieve data from our server.
--- Unit-4/block31_backend_fundamentals/guided_practice/index.js
+++ Unit-4/block31_backend_fundamentals/guided_practice/index.js
@@ -1,47 +1,33 @@
-const express = require('express');
-const app = express();
-
-const PORT = 8000 || 8080;
+// Create Express Server at port 8080
 
 app.get('/', (req, res) => {
-    // server up the index.html
-    res.sendFile(__dirname + '/index.html');
+    // server up the public folder as static index.html file
+
 });
+
 
 // HELLO WORLD endpoint
 app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+    // send a response
+    
 });
 
 // dynamic endpoint
 app.get('/hello/:name', (req, res) => {
     // get the name from the request
-    const name = req.params.name;
-    // send a response
-    res.send(`Hello ${name}`);
-});
 
-// dynamic endpoint with two params
-app.get('/hello/:name/:age', (req, res) => {
-    // get the name from the request
-    const name = req.params.name;
-    const age = req.params.age;
     // send a response
-    res.send(`Hello ${name}, you are ${age} years old`);
+
 });
 
 // dynamic endpoint with query string
 app.get('/hello-query', (req, res) => {
     // get the name from the request
-    const name = req.query.name;
+
     // send a response
-    res.send(`Hello ${name}`);
+
 });
 
-// show port number in console
-// commented out for testing
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// set the server to listen on port 8080
 
 module.exports = app;
