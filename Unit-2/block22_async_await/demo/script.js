3763907a3dd5552a0348cdedc922862d58f674db
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-03-21 05:56:22 -0500

    MVP demo up

--- Unit-2/block22_async_await/demo/script.js
+++ Unit-2/block22_async_await/demo/script.js
@@ -1,55 +1,180 @@
-// Get the recipe list container element
-const recipeList = document.querySelector('.recipe-list');
-const recipeForm = document.querySelector('#recipe-form');
+// get elements
+const recipesContainer = document.getElementById('recipes-container');
+const newRecipeFormContainer = document.getElementById('new-recipe-form');
 
-// Make a GET request to the RESTful API endpoint
-fetch('http://localhost:8080/api/demo/recipes')
-    .then(response => response.json())
-    .then(data => {
-        // Loop through the recipe items and create the HTML for each item
-        data.forEach(recipe => {
-            const recipeItem = `
-        <div class="recipe-item">
-          <img class="recipe-image" src="${recipe.image_url}">
-          <div class="recipe-title">${recipe.title}</div>
-          <div class="recipe-description">${recipe.description}</div>
-          <a href="#" class="recipe-link">View Recipe</a>
-        </div>
-      `;
-            // Add the recipe item to the recipe list container
-            recipeList.innerHTML += recipeItem;
+// API URL
+const API_URL = 'http://localhost:8080/api/demo/recipes';
+
+// fetch recipes
+const fetchAllRecipes = async () => {
+    try {
+        const response = await fetch(API_URL);
+        const recipes = await response.json();
+        console.log(recipes);
+        return recipes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// fetch single recipe
+const fetchSingleRecipe = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/${id}`);
+        const recipe = await response.json();
+        const recipeElement = document.createElement('div');
+        recipeElement.classList.add('recipe');
+        recipeElement.innerHTML = `
+            <h4>${recipe.title}</h4>
+            <p>${recipe.instructions}</p>
+        `;
+        recipesContainer.appendChild(recipeElement);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// create new recipe
+const createNewRecipe = async (title, image_url, instructions) => {
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            body: JSON.stringify({ title, image_url, instructions }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
         });
-    })
-    .catch(error => {
-        console.error('Error fetching recipes:', error);
-    });
+        const recipe = await response.json();
+        console.log(recipe);
+        fetchAllRecipes();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-recipeForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+// remove recipe
+const removeRecipe = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/${id}`, {
+            method: 'DELETE',
+        });
+        const recipe = await response.json();
+        console.log(recipe);
+        fetchAllRecipes();
 
-    const titleInput = document.getElementById('title');
-    const titleValue = titleInput.value.trim(); // Trim leading and trailing whitespace
+        // reload the window
+        window.location.reload();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-    if (!titleValue) {
-        console.log('Title field cannot be empty');
+// // render all recipes
+const renderAllRecipes = (recipeList) => {
+    if (!recipeList || recipeList.length === 0) {
+        recipesContainer.innerHTML = '<h3>No recipes found</h3>';
         return;
     }
 
-    const formData = new FormData(recipeForm);
+    recipesContainer.innerHTML = '';
 
-    fetch('http://localhost:8080/api/demo/recipes', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => {
-            if (response.ok) {
-                console.log('Recipe added successfully');
-                recipeForm.reset();
-            } else {
-                console.log('Error adding recipe');
-            }
-        })
-        .catch(error => {
-            console.error('Error submitting form:', error);
+    recipeList.forEach((recipe) => {
+        const recipeElement = document.createElement('div');
+        recipeElement.classList.add('recipe-card');
+        recipeElement.innerHTML = `
+            <h4>${recipe.title}</h4>
+            <img src="${recipe.image_url}" alt="${recipe.title}">
+            <p>${recipe.instructions}</p>
+            <button class="delete-button" data-id="${recipe.id}">Remove</button>
+            <button class="detail-button" data-id="${recipe.id}">See Details</button>
+        `;
+        recipesContainer.appendChild(recipeElement);
+
+        let deleteButton = recipeElement.querySelector('.delete-button');
+        deleteButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            removeRecipe(recipe.id);
         });
-});
+
+        let detailButton = recipeElement.querySelector('.detail-button');
+        detailButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            renderSingleRecipe(recipe);
+        });
+    });
+
+
+}
+
+// render single recipe
+const renderSingleRecipe = (recipe) => {
+    if (!recipe || recipe.length === 0) {
+        recipesContainer.innerHTML = '<h3>No recipe found</h3>';
+        return;
+    }
+
+    let recipeHTML = `
+        <div class="single-recipe-view">
+            <div class="recipe">
+                <h4>${recipe.title}</h4>
+                <img src="${recipe.image_url}" alt="${recipe.title}">
+                <p>${recipe.instructions}</p>
+            </div>
+
+            <button class="back-button">Back</button>
+        </div>`;
+    recipesContainer.innerHTML = recipeHTML;
+
+    let backButton = recipesContainer.querySelector('.back-button');
+    backButton.addEventListener('click', async () => {
+        const recipes = await fetchAllRecipes();
+        renderAllRecipes(recipes);
+    });
+
+}
+
+// create new recipe form
+const createNewRecipeForm = () => {
+    let formHtml = `
+    <form>
+    <label for="title">Title</label>
+    <input type="text" id="title" name="title" placeholder="Title">
+    <label for="image_url">Image URL</label>
+    <input type="text" id="image_url" name="image_url" placeholder="Image URL">
+    <label for="instructions">Instructions</label>
+    <textarea id="instructions" name="instructions" placeholder="Instructions"></textarea>
+    <button type="submit">Create</button>
+    </form>
+    `;
+    newRecipeFormContainer.innerHTML = formHtml;
+
+    let form = newRecipeFormContainer.querySelector('form');
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        let recipeData = {
+            title: form.title.value,
+            image_url: form.image_url.value,
+            instructions: form.instructions.value
+        };
+
+        await createNewRecipe(recipeData.title, recipeData.image_url, recipeData.instructions);
+
+        const recipes = await fetchAllRecipes();
+        renderAllRecipes(recipes);
+
+        form.title.value = '';
+        form.image_url.value = '';
+        form.instructions.value = '';
+    });
+}
+
+const init = async () => {
+    const recipes = await fetchAllRecipes();
+    renderAllRecipes(recipes);
+
+    createNewRecipeForm();
+}
+
+init();
+
