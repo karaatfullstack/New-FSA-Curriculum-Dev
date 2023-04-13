9bb6fcba04adc30bb051a155216699a75a6b3c20
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2023-04-13 12:26:53 -0500

    unit 4 starter / solution code spun out

--- Unit-4/block32_API_routes/guided_practice/solution/db/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    ...require('./dogs'),
-    ...require('./cats')
-};
--- Unit-4/block33_SQL/guided_practice/solution/db/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // ...require('./client'), // adds key/values from users.js
-  ...require('./users'), // adds key/values from users.js
-  ...require('./articles'), // adds key/values from articles.js
-}
