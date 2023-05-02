e8cde1c5bfdb822cbcf2c37c289ea40ff62db8bf
sagar11sachin <124698334+sagar11sachin@users.noreply.github.com>
2023-05-02 19:02:51 +0530

    fixed typos, camelCasing, and beautified.

--- Unit-2/block_19A_Writing_JavaScript_Tests/Demo/Demo_Calculator_Testing/calculator.test.js
+++ Unit-2/block_19A_Writing_JavaScript_Tests/Demo/Demo_Calculator_Testing/calculator.test.js
@@ -1,5 +1,3 @@
-
-
 const calculations = require('./calculator');
 
 // writing differnet test cases
@@ -7,21 +5,21 @@ const calculations = require('./calculator');
 describe("Calculator tests", () => {
     test('adding 1 + 2 should return 3', () => {
       expect(calculations.sum(1, 2)).toBe(3);
-    });
+      });
 
     test('subtracting 17 - 5 should return 12', () => {
         expect(calculations.diff(17, 5)).toBe(12);
       });
     
-      test('multiplying 7*8 should return 56', () => {
-        expect(calculations.product(7, 8)).toBe(56);
+    test('multiplying 7*8 should return 56', () => {
+      expect(calculations.product(7, 8)).toBe(56);
       });
     
-      test('multiplying 3*4 should return 12', () => {
-        expect(calculations.product(3, 4)).toBeCloseTo(12);
+    test('multiplying 3*4 should return 12', () => {
+      expect(calculations.product(3, 4)).toBeCloseTo(12);
       });
 
-      test('dividing 12/3 should return 4', () => {
-        expect(calculations.divide(12, 3)).toBeCloseTo(4);
+    test('dividing 12/3 should return 4', () => {
+      expect(calculations.divide(12, 3)).toBeCloseTo(4);
       });
    })
