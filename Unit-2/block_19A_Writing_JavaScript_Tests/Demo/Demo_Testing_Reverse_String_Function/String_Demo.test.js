cbd9ae019704813582bdefcd031bd27de089ecdc
sagar11sachin <124698334+sagar11sachin@users.noreply.github.com>
2023-05-02 19:10:02 +0530

    updates comments and beautified.

--- Unit-2/block_19A_Writing_JavaScript_Tests/Demo/Demo_Testing_Reverse_String_Function/String_Demo.test.js
+++ Unit-2/block_19A_Writing_JavaScript_Tests/Demo/Demo_Testing_Reverse_String_Function/String_Demo.test.js
@@ -1,16 +1,21 @@
-const reverseString = require('./String_Demo');
+// Import the reverseString function from the String_Demo.js file
+const reverseString = require("./String_Demo");
 
+// Define a group of tests using Jest's describe function
 describe("Calculator tests", () => {
+  // Define a test case using Jest's test function
+  test("reverseString reverses a string worked correctly", () => {
+    // Call the reverseString function with the input 'hello' and expect the output to be 'olleh'
+    expect(reverseString("hello")).toBe("olleh");
+  });
 
-test('reverseString reverses a string worked correctly', () => {
-  expect(reverseString('hello')).toBe('olleh');
-});
+  test("reverseString reverses a string with spaces worked correctly", () => {
+    // Call the reverseString function with the input 'hello world' and expect the output to be 'dlrow olleh'
+    expect(reverseString("hello world")).toBe("dlrow olleh");
+  });
 
-test('reverseString reverses a string with spaces worked correctly', () => {
-  expect(reverseString('hello world')).toBe('dlrow olleh');
+  test("reverseString reverses a string with punctuation worked correctly", () => {
+    // Call the reverseString function with the input 'hello, world!' and expect the output to be '!dlrow ,olleh'
+    expect(reverseString("hello, world!")).toBe("!dlrow ,olleh");
+  });
 });
-
-test('reverseString reverses a string with punctuation worked correctly', () => {
-  expect(reverseString('hello, world!')).toBe('!dlrow ,olleh');
-});
-})
