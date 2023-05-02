c1bf0519a052ed98648c8ea037a4d462b19a59d8
sagar11sachin <124698334+sagar11sachin@users.noreply.github.com>
2023-05-02 19:05:26 +0530

    updated comments and beautified

--- Unit-2/block_19A_Writing_JavaScript_Tests/Demo/Demo_Testing_Reverse_String_Function/String_Demo.js
+++ Unit-2/block_19A_Writing_JavaScript_Tests/Demo/Demo_Testing_Reverse_String_Function/String_Demo.js
@@ -1,12 +1,19 @@
 // Function to reverse a given string
 function reverseString(str) {
-    let newString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    return newString;    
+  // Create a new empty string to store the reversed version of the input string
+  let newString = "";
+
+  // Iterate over the characters in the input string in reverse order,
+  // starting from the last character (i.e., str.length - 1)
+  // and ending with the first character (i.e., index 0)
+  for (let i = str.length - 1; i >= 0; i--) {
+    // Add each character to the new string in reverse order
+    newString += str[i];
+  }
+
+  // Return the reversed string
+  return newString;
 }
 
-// Exproting the function  
+// Exproting the function
 module.exports = reverseString;
-  
