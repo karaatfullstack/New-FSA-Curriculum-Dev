594631f7558c408e368eb85a560511d84c6d7e5e
Simplilearnprachi <124772462+Simplilearnprachi@users.noreply.github.com>
2023-05-08 13:03:20 +0530

    Update First_class_Function.js

--- Unit-3/Block 24A Functional_programming/Block_24A_Workshop/First_class_Function.js
+++ Unit-3/Block 24A Functional_programming/Block_24A_Workshop/First_class_Function.js
@@ -14,3 +14,27 @@ function multiplierDisplay(a) {
 }
 const double = multiplierDisplay(3);
 console.log(double(7));
+
+// Returning functions
+const adder = (x) => (y) => {
+  const result = x + y;
+  console.log(`Adding ${x} and ${y} gives ${result}`);
+  return result;
+};
+
+// Here is the same function written without arrow syntax
+
+function otherAdder(x) {
+  return function (y) {
+    const result = x + y;
+    console.log("Adding " + x + " and " + y + " gives " + result);
+    return result;
+  };
+}
+
+const add2 = adder(2);
+const add4 = adder(4);
+
+const num1 = add2(5); // What will num1 be?
+const num2 = add4(5);
+
